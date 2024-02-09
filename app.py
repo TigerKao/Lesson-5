eac22260690005849ee9a921d26fe7c36886e0e8
Susanoo Kao <tiger.tzuhsunkao@gmail.com>
2024-02-09 12:31:06 +0800

    read csv

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 products = []
 
+with open("product.csv", "r", encoding = "utf-8") as f:
+    for line in f:
+        name, price = line.strip().split(",") # split 完的結果是清單
+        products.append([name, price])
+print(products)
+
+# [['商品', ' 價格'], ['ramen', '280'], ['pasta', '200']]
+
 while True:
     name = input("請輸入商品名稱：")
     if name == "q":
@@ -13,6 +21,7 @@ products[0][0]
 for product in products:
     print(product[0])
 
-with open("product.csv", "w") as f:
+with open("product.csv", "w", encoding = "utf-8") as f:
+    f.write("商品, 價格\n")
     for product in products:
         f.write(product[0] + "," + str(product[1]) + "\n")
